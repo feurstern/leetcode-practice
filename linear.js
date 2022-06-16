c7de093aa1097d23e16259a28bd22c0e3e12fa8d
feuerstern <rio-kurniawan@sakura-schutzstaffel.com>
2022-06-16 19:58:50 +0700

    Adding some coding challenges

---
+++ linear.js
@@ -0,0 +1,14 @@
+const quadratic=(n)=>{
+    for (let i=0; i<n; i++){
+        console.log(i);
+
+        for(let j=i;j<n;j++){
+            console.log(j);
+
+        }
+
+        
+    }
+}
+
+quadratic(80);
